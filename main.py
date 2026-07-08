7b66eaddd4649b71a679de82c0a7542b0bcba915
kyrian <benkay599@gmail.com>
2026-07-08 06:25:12 -0700

    entered new varriables

--- main.py
+++ main.py
@@ -39,10 +39,22 @@ print(person.fight())
 
 class Female(Human):
     def cry(self):
+        if self.age >= 30 and self.age <= 59:
+            return f"my name is {self.name}.i cry even tho i am an old woman"
+        elif self.age >= 50:
+            return f"my name is {self.name}.i cry even tho my mates have grand children"
         return f"my name is {self.name}. I cry because i am a woman"
+        
+    def dance(self):
+        dance_steps = [1, 2, 3, 4, 5]     
+        dance = []
+        for step in dance_steps:
+            dance.append(f"dance step {step}")
+        return dance
 
-new_person = Female("Ada",24)        
+new_person = Female("Ada",41)        
 
 print(new_person)
 print(new_person.cry())
-
+print(new_person.dance())
+print(", ".join(new_person.dance()))
